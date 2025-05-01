6b12bcff135a8387eb293e11a7906f295c5b35fd
nayerfki <nayerfki123@gmail.com>
2025-05-01 18:48:29 +0100

    Remove frontend/src/setupTests.js

--- frontend/src/setupTests.js
+++
@@ -1,8 +0,0 @@
-// src/setupTests.js
-import { setupServer } from 'msw/node';
-import handlers from './handlers'; // Import your handlers
-
-const server = setupServer(...handlers);
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
